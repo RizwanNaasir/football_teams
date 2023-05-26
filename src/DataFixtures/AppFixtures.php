f899b7b2c94b05896f14cb7c20b9dbfab28c8b9c
Rizwan Nasir <97438351+RizwanNaasir@users.noreply.github.com>
2023-05-26 13:03:15 +0500

    added: more data in fixture
    Signed-off-by: Rizwan Nasir <97438351+RizwanNaasir@users.noreply.github.com>

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,19 +12,34 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         // Create a team
-        $team = new Team();
-        $team->setName('Sample Team');
-        $team->setCountry('Sample Country');
-        $team->setMoneyBalance(1000000);
+        $team1 = new Team();
+        $team1->setName('Sample Team');
+        $team1->setCountry('Sample Country');
+        $team1->setMoneyBalance(1000000);
         // Add more fixtures as needed...
 
-        $player = new Player();
-        $player->setName('Sample Player');
-        $player->setSurName('Sample SurName');
-        $player->setTeam($team);
+        $player1 = new Player();
+        $player1->setName('Sample Player');
+        $player1->setSurName('Sample SurName');
+        $player1->setTeam($team1);
 
-        $manager->persist($team);
-        $manager->persist($player);
+        $manager->persist($team1);
+        $manager->persist($player1);
+
+
+        $team2 = new Team();
+        $team2->setName('Sample Team 2');
+        $team2->setCountry('Sample Country 2');
+        $team2->setMoneyBalance(1000000);
+
+        $player2 = new Player();
+        $player2->setName('Sample Player 2');
+        $player2->setSurName('Sample SurName 2');
+        $player2->setTeam($team2);
+
+        $manager->persist($team2);
+        $manager->persist($player2);
+
         $manager->flush();
     }
 }
